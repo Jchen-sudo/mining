ad33ce7b7fac5c33377e45d6a888354aaf089058
framist <framist@163.com>
2022-06-12 22:43:23 +0800

    避免flask debug时线程双次启动

--- web_view_testing/run.py
+++ web_view_testing/run.py
@@ -1,11 +1,19 @@
-#coding:UTF-8
-
+# coding:UTF-8
 from app import app,sniff_p
-# sniff 进程
 
+WELCOME_MSG = '''
+  __  __ _       _        _____                           
+ |  \/  (_)     (_)      / ____|                          
+ | \  / |_ _ __  _ _ __ | |  __  _____   _____ _ __ _ __  
+ | |\/| | | '_ \| | '_ \| | |_ |/ _ \ \ / / _ \ '__| '_ \ 
+ | |  | | | | | | | | | | |__| | (_) \ V /  __/ |  | | | |
+ |_|  |_|_|_| |_|_|_| |_|\_____|\___/ \_/ \___|_|  |_| |_|
+                                                          
+'''
 
 HAVEN_NO_SNIFF = False
 if __name__ == '__main__':
+    print(WELCOME_MSG)
     if HAVEN_NO_SNIFF:
         sniff_p.start()
-    app.run(host='0.0.0.0', port=8081)
+    app.run(host='0.0.0.0', port=8081, use_reloader=False, debug=False)
